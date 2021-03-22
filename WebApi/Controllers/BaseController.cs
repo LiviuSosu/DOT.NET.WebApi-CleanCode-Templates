e32fe6b05a7080b75616a5b1615dc8f319e4c0db
LiviuSosu <sosuliviu@gmail.com>
2021-03-22 22:49:03 +0100

    added the base controller

--- WebApi/Controllers/BaseController.cs
+++ WebApi/Controllers/BaseController.cs
@@ -1,12 +1,16 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace WebApi.Controllers
 {
     public class BaseController : Controller
     {
+        private IMediator _mediator;
+
+        protected IMediator Mediator => _mediator ??= HttpContext.RequestServices.GetService<IMediator>();
+        protected const int badRequestErrorCode = 400;
+        protected const int notFoundErrorCode = 404;
+        protected const int internalServerErrorCode = 500;
     }
 }
