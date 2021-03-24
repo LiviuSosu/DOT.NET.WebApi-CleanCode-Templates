5ed501b7e716829011a464143b4bc8426472c175
LiviuSosu <sosuliviu@gmail.com>
2021-03-24 22:50:28 +0100

    added the configuration implementaton

--- Common/Configuration.cs
+++ Common/Configuration.cs
@@ -1,14 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.Extensions.Configuration;
+using System.IO;
 
 namespace Common
 {
     public class Configuration : IConfiguration
     {
+        private readonly string loggingFilePath;
+        public string LoggingFilePath { get => loggingFilePath; }
+
         private readonly string errorMessage;
         public string ErrorMessage { get => errorMessage; }
+
+        public Configuration()
+        {
+            string path = Path.Combine(Directory.GetCurrentDirectory(), "appsettings.json");
+
+            var configurationBuilder = new ConfigurationBuilder();
+            configurationBuilder.AddJsonFile(path, false);
+            var root = configurationBuilder.Build();
+
+            loggingFilePath = root.GetSection("Logging").GetSection("loggingFilePath").Value;
+
+            errorMessage = root.GetSection("Errors").GetSection("DisplayGenericUserErrorMessage").Value;
+        }
     }
 }
